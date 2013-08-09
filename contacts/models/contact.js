18d2322843b410a45f32b4689ae50daa6e0959f4
Sam Warren <sam.warren@frogtrade.com>
2013-08-09 17:20:37 +0100

    Made live binding work properly

--- contacts/models/contact.js
+++ contacts/models/contact.js
@@ -1,5 +1,11 @@
 app.Contacts_Model_Contact = can.Model({
     findAll: 'GET /contacts'
 },{
+    init: function() {
+        if (!this.count) {
+            this.attr('count', 0);
+        }
 
+        console.log(this);
+    }
 });
